2275000601dd422eadfd1cfea90890e802f1ce5b
JS-Develope <jeanaparecido21@gmail.com>
2024-09-22 10:03:16 -0300

    refactor: removing unused validation commander

--- src/decks/validation/validation-commander-deck.ts
+++ src/decks/validation/validation-commander-deck.ts
@@ -7,10 +7,6 @@ export class ValidationCommanderDeck implements ValidationDeckHandler {
     private next: ValidationDeckHandler = null; 
 
     handle(deck: ValidateDeckDTO): void {
-
-        if (!deck.commander) {
-            throw new NotAcceptableException("Deck inválido, é necessário ter um commander.");
-        }
         
         if (!deck.commander.supertypes?.includes('Legendary')) {
             throw new NotAcceptableException("Deck inválido, um commander precisa ter um supertype de Legendary.");
