73b0686443306bc28d459c49d6c92bbd9233f07d
JS-Develope <jeanaparecido21@gmail.com>
2024-09-28 19:28:49 -0300

    feat: diminuindo tempo de população ao usar stream

--- src/utils/factories/card-factory.ts
+++ src/utils/factories/card-factory.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import CardAdapter from "src/decks/adapter/card.adapter";
-import { ResponseCardDto } from "src/decks/dto/response-card.dto";
+import { CreateCardDto } from "src/decks/dto/create-card.dto";
+import { ReadableStream } from "stream/web";
 import { CardRepository } from '../../decks/card.repository';
 import { MagicRequest } from "../request/magic.request";
 
@@ -15,40 +16,82 @@ export class CardFactory {
 
     public async populate(): Promise<void> {
 
-        console.time('Tempo de população:')
-
-        let count = 1;
-        let urls: string[] = [];
-        let batchSize = 25;
-        let exists = true;
-
-        while (exists) {
-
-            const url: string = this.request.getUrlSimple(count);
-            urls.push(url);
-
-            if (urls.length !== batchSize) {
-                count++;
-                continue;
-            }
-                
-            const promises = urls.map(async (url) => {
-                const response: ResponseCardDto = await this.fetchData(url);
-                exists = response.cards.length !== 0;
-                return response.cards.map(card =>  this.adapter.createToEntity(card));
-            });
-
-            const batchResults = await Promise.all(promises);
-
-            this.repository.createAll(batchResults.flat());
-            urls = [];
-            count++;
-        }
-
-        console.timeEnd('Tempo de população:')
+        console.time('Time');
+    
+        const readableStream = await this.createPaginatedStream(1, 313);
+        const readableStream2 = await this.createPaginatedStream(314, 627);
+        const readableStream3 = await this.createPaginatedStream(628, 937);
+    
+        await Promise.all([
+            this.consumePaginatedStream(readableStream),
+            this.consumePaginatedStream(readableStream2),
+            this.consumePaginatedStream(readableStream3),
+        ]);
+    
+        console.timeEnd('Time');
     }
 
-    private async fetchData(url: string): Promise<ResponseCardDto> {
-        return await fetch(url).then((response) => response.json());
+    private async createPaginatedStream(currentPage: number, totalPages: number): Promise<ReadableStream<any>> {
+
+        const readableStream = new ReadableStream({
+            pull: async (controller) => {
+                if (currentPage > totalPages) {
+                    controller.close();
+                    return;
+                }
+
+                const fetchPromises = [];
+                const maxConcurrentRequests = 30;
+
+                while (fetchPromises.length < maxConcurrentRequests && currentPage <= totalPages) {
+                    fetchPromises.push(this.fetchPageData(currentPage));
+                    currentPage++;
+                }
+
+                try {
+                    const results = await Promise.all(fetchPromises);
+                    results.forEach(data => {
+                        controller.enqueue(data);
+                    });
+                } catch (error) {
+                    controller.error(error);
+                }
+            }
+        });
+
+        return readableStream;
+    }
+
+    private async fetchPageData(page: number, retries: number = 3): Promise<any> {
+        console.log(`Buscando página: ${page}`);
+        await new Promise(resolve => setTimeout(resolve, 50));
+    
+        const response = await fetch(this.request.getUrlSimple(page));
+    
+        if (!response.ok) {
+            if (retries > 0) {
+                console.warn(`Erro em página: ${page}`);
+                return this.fetchPageData(page, retries - 1);
+            }
+
+            return null;
+        }
+    
+        const data = await response.json();
+        return data;
+    }
+
+    private async consumePaginatedStream(stream: ReadableStream<any>) {
+        const reader = stream.getReader();
+        let done: boolean;
+
+        do {
+            const { value, done: isDone } = await reader.read();
+            done = isDone;
+
+            if (value) {
+                this.repository.createAll(value?.cards?.map((card: CreateCardDto) => this.adapter.createToEntity(card)));
+            }
+        } while (!done);
     }
 }
