bd012865c1824873be284893274233afdc20d2ec
JS-Develope <jeanaparecido21@gmail.com>
2024-11-30 21:16:28 -0300

    fix: isolando modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitMQModule } from './config/rabbitmq/rabbitmq.module';
 import { RedisModule } from './config/redis/redis.module';
 import { DecksModule } from './decks/decks.module';
 import { UsersModule } from './users/users.module';
@@ -19,6 +20,7 @@ const mongooseConfig = (configService: ConfigService) => ({
       useFactory: mongooseConfig,
     }),
     DecksModule,
+    RabbitMQModule,
   ],
 })
 export class AppModule {}
--- src/decks/decks.module.ts
+++ src/decks/decks.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthService } from 'src/config/auth/auth.service';
+import { RabbitMQModule } from 'src/config/rabbitmq/rabbitmq.module';
 import { RedisService } from 'src/config/redis/redis.service';
+import { NotificationsModule } from 'src/notifications/notification.module';
+import { NotificationService } from 'src/notifications/notification.service';
+import { UsersModule } from 'src/users/users.module';
 import { UsersRepository } from 'src/users/users.repository';
 import { CardFactory } from 'src/utils/factories/card-factory';
 import { DecksFactory } from 'src/utils/factories/decks-factory';
@@ -12,35 +17,13 @@ import { DecksRepository } from './decks.repository';
 import { DecksService } from './decks.service';
 import { Card, CardsSchema } from './entities/card.entity';
 import { Deck, DecksSchema } from './entities/deck.entity';
-import { UsersModule } from 'src/users/users.module';
-import { AuthService } from 'src/config/auth/auth.service';
-import { NotificationService } from 'src/notifications/notification.service';
-import { NotificationsModule } from 'src/notifications/notification.module';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Deck.name, schema: DecksSchema }, { name: Card.name, schema: CardsSchema }]),
     UsersModule,
     NotificationsModule,
-    ClientsModule.registerAsync([
-      {
-        name: 'Notifications_service',
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.RMQ,
-          options: {
-            urls: [`${configService.get<string>('RABBITMQ_URL')}:${configService.get<string>('RABBITMQ_PORT')}`],
-            queue: configService.get<string>('RABBITMQ_QUEUE'),
-            queueOptions: {
-              durable: false
-            },
-          },
-        }),
-      },
-    ]),
+    RabbitMQModule
   ],
   providers: [DecksService, DecksRepository, DecksFactory, MagicRequest, CardAdapter, RedisService, CardRepository, UsersRepository, CardFactory, AuthService, NotificationService],
   controllers: [DecksController],
